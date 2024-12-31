3a4201a2192ebf0db8bfa5a92c2d1b14a37f0966
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-12-31 02:27:20 -0500

    remove unused intents

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,6 @@ const client = new CoreClient({
     intents: [
       Intents.FLAGS.GUILDS,
       Intents.FLAGS.GUILD_MEMBERS,
-      Intents.FLAGS.GUILD_INTEGRATIONS,
-      Intents.FLAGS.GUILD_WEBHOOKS,
-      Intents.FLAGS.GUILD_VOICE_STATES,
-      Intents.FLAGS.GUILD_PRESENCES,
       Intents.FLAGS.GUILD_MESSAGES,
       Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     ],
