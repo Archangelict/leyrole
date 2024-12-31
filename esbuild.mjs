16605eb4f9e0b82350e14b2f8fc0115d0024dd11
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-12-31 02:26:28 -0500

    fix esbuild script

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,20 +1,29 @@
-import esbuild from 'esbuild'
-import fs from 'fs'
+import esbuild from "esbuild";
+import fs from "fs";
+import { basename } from "path";
 
 await esbuild.build({
-  entryPoints: ['src/index.ts'],
+  entryPoints: ["src/index.ts"],
   bundle: true,
-  outdir: 'dist',
-    platform: 'node',
+  outdir: "dist",
+  keepNames: true,
+  platform: "node",
+  format: "cjs",
   // eslint-disable-next-line no-undef
-  minify: (process.env.ESBUILD_MINIFY ?? '') !== '',
-})
+  minify: (process.env.ESBUILD_MINIFY ?? "") !== "",
+});
 
-for (const bot of fs.readdirSync('src/bots')) {
-    await esbuild.build({
-        entryPoints: ['src/bots/' + bot + '/**/*.ts'],
-        outdir: 'dist/bots/' + bot,
-        // eslint-disable-next-line no-undef
-        minify: (process.env.ESBUILD_MINIFY ?? '') !== '',
-      })
+for (const bot of fs.readdirSync("src/bots")) {
+  await esbuild.build({
+    entryPoints: ["src/bots/" + bot + "/**/*.ts"],
+    outdir: "dist/bots/" + bot,
+    keepNames: true,
+    platform: "node",
+    format: "cjs",
+    // eslint-disable-next-line no-undef
+    minify: (process.env.ESBUILD_MINIFY ?? "") !== "",
+  });
+  for (const file of fs.globSync("src/bots/" + bot + "/**/*.json")) {
+    fs.copyFileSync(file, "dist/bots/" + bot + "/" + basename(file));
+  }
 }
